06c00f98e5982a4cef5ce734a9b41f7aff5a9a27
elidotco <eddiefestde@gmail.com>
2026-05-22 00:17:33 +0000

    dfhg

--- sections/Packages.tsx
+++ sections/Packages.tsx
@@ -5,7 +5,7 @@ const Packages = () => {
     <section className="min-h-[70vh]">
       <div className="flex justify-between  mx-auto px-10 items-center max-w-7xl">
         <h2 className="lg:text-2xl">
-          Packages for your special ones on that special day{" "}
+          {/* Packages for your special ones on that special day{" "} */}
         </h2>
       </div>
     </section>
